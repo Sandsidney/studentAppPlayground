c5719afcb6823e86a45726304dd3b7ab9d58de74
BHSRam9 <sandsidn@badinhs.org>
2017-01-09 07:44:32 -0500

    final Commit

---
+++ Contents.swift
@@ -0,0 +1,7 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var mainPageTableView = ["Bell Scheduale", "Scheduale", "Clubs & Activities", "Spirit Shop", "Student of the month", "Cafeteria", "Campus Ministry", "M & M Activity", "Daily Anouncements", "Scholarship Opportunities"]
+
+
